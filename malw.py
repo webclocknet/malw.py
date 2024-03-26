079722cebd92ddd9036fcaafc4375da9f94c662c
webclocknet <96979468+webclocknet@users.noreply.github.com>
2024-03-26 18:10:37 +0200

    Update malw.py

--- malw.py
+++ malw.py
@@ -1,39 +1,29 @@
 import os
 import subprocess
 import sys
-import ctypes
-
-def is_admin():
-    try:
-        return os.getuid() == 0
-    except AttributeError:
-        return ctypes.windll.shell32.IsUserAnAdmin() != 0
 
 def execute_files(folder_path):
-    file_paths = []
+    file_paths = set()
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith(('.exe', '.msi', '.vbs', '.hta', '.cmd')):
-                file_path = os.path.join(root, file)
-                file_paths.append(file_path)
+                file_path = os.path.abspath(os.path.join(root, file))
+                file_paths.add(file_path)
     
-    print(f"{len(file_paths)} files found")
+    if len(file_paths) == 0:
+        print('No files found in folder "Malware". Add executables into the folder named "Malware" to execute.')
+    elif len(file_paths) == 1:
+        print(f"{len(file_paths)} file found")
+    else:
+        print(f"{len(file_paths)} files found")
+    processes = []
     for file_path in file_paths:
         print(f"{file_path}")
-        subprocess.call([file_path], shell=True)
+        process = subprocess.Popen([file_path], shell=True)
+        processes.append(process)
+    
+    for process in processes:
+        process.wait()
 
 if __name__ == "__main__":
-    if not is_admin():
-        print("enable admin privileges? (y/n)")
-        response = input().lower()
-        if response == 'y':
-            if sys.platform == "win32":
-                ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-            else:
-                print("Run this script as admin.")
-                sys.exit()
-        else:
-            print("exit")
-            sys.exit()
-    
     execute_files("Malware")
